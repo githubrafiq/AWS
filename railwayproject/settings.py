13d31ae5abe51ef039a0fc66c27e44921f1bfa52
rafiq <rafiq.freelancer24@gmail.com>
2023-01-23 10:56:01 +0800

    postgresql added

--- railwayproject/settings.py
+++ railwayproject/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'railwayproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'RAILWAY-DATA',
+        'USER': 'postgres',
+        'PASSWORD': 121995,
+        'HOST': 'localhost',
+        'PORT': 5432,
+
     }
 }
 
