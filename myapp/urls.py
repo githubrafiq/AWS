7de3853f317adb09f329bb9ddb709cb8849bee9e
rafiq <rafiq.freelancer24@gmail.com>
2023-01-25 13:57:09 +0800

    favicon

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('favicon/', RedirectView.as_view(url='/static/images/favicon.ico')),
 ]
